730fa2548492a71027263d63bfcac751d9ca2b6e
Jakub Jarosz <jakub.jarosz@postpro.net>
2016-02-14 19:39:15 +0000

    quiz, dotproduct

--- quiz/product.py
+++ quiz/product.py
@@ -18,4 +18,11 @@ This function takes in two lists of numbers and returns a number.
 """
 
 
+def dotProduct(listA, listB):
+    '''
+    listA: a list of numbers
+    listB: a list of numbers of the same length as listA
 
+    '''
+    return sum([listA[i] * listB[i] for i in range(len(listA))])
+ 
