4c2f0a8cf3156f518bac01c71193fb7226c81e5d
Jakub Jarosz <jakub.jarosz@postpro.net>
2016-03-01 19:52:19 +0000

    L12 problem 1 - final

--- week-6/multi.py
+++ week-6/multi.py
@@ -2,31 +2,42 @@
 
 
 class A(object):
+
     def __init__(self):
         self.a = 1
+
     def x(self):
         print "A.x"
+
     def y(self):
         print "A.y"
+
     def z(self):
         print "A.z"
+
 
 class B(A):
     def __init__(self):
         A.__init__(self)
         self.a = 2
         self.b = 3
+
     def y(self):
         print "B.y"
+
     def z(self):
         print "B.z"
 
+
 class C(object):
+
     def __init__(self):
         self.a = 4
         self.c = 5
+
     def y(self):
         print "C.y"
+
     def z(self):
         print "C.z"
 
@@ -41,5 +52,13 @@ class D(C, B):
 
 
 if __name__=="__main__":
-    pass
+    obj = D()
+    print("obj = D()")
+    print("obj.a : {}".format(obj.a))
+    print("obj.b : {}".format(obj.b))
+    print("obj.c : {}".format(obj.c))
+    print("obj.d : {}".format(obj.d))
+    print("obj.x() : {}".format(obj.x()))
+    print("obj.y() : {}".format(obj.y()))
+    print("obj.z() : {}".format(obj.z()))
 
